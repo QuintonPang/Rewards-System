8764289b1b00e7cbe11dd27115c3a58611fa98e2
CHOO MUN CHUN <choomc-wp22@student.tarc.edu.my>
2024-05-08 08:13:25 +0800

    contact management

---
+++ src/Assignment/Account.java
@@ -0,0 +1,26 @@
+public abstract class Account {
+
+    public Account(String username, String password, String email, String phone, String membershipNumber,String transactionRecord) {
+    }
+  
+    public void setUsername(String username) {
+    }
+  
+    public void setPassword(String password) {
+    }
+  
+    public void setEmail(String email) {
+    }
+  
+    public void setPhone(String phone) {
+    }
+
+    public void setMembershipNumber(String membershipNumber) {
+    }
+
+    public void setTransactionRecord(String transactionRecord) {
+    }
+  
+    public abstract void displayMenu();
+  }
+  
