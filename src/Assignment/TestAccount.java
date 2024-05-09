dfb9b156a8af60d4b880780359471ca39be676ce
yewwk <121840614+yewwk@users.noreply.github.com>
2024-05-09 09:40:29 +0800

    Delete src/Assignment/TestAccount.java

--- src/Assignment/TestAccount.java
+++
@@ -1,6 +0,0 @@
-public class TestAccount {
-    public static void main(String[] args) {
-        AccountOperations userAccount = new UserAccount(); // Use the interface type
-        userAccount.displayMenu(); // Invoke methods through the interface
-    }
-}
